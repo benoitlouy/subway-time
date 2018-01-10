8383ef56601ebc43f81f13a13150117c5a282708
Benoit Louy <benoit.louy@fastmail.com>
2018-01-10 14:44:10 -0500

    refactor

--- subway_time/__main__.py
+++ subway_time/__main__.py
@@ -16,6 +16,9 @@ class Config:
         self.stop_ids = config["mta"]["stop_ids"]
 
 def main():
+
+    fps = 1
+
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument("--config", "-c", required = True)
     args = arg_parser.parse_args()
@@ -25,20 +28,22 @@ def main():
     font_path = get_resource("helvR08.pil")
     font = ImageFont.load(font_path)
     fetcher = MTASubwayFetcher(config.api_key, config.feed_ids, config.stop_ids)
+
+    while True:
     stop_info = fetcher.fetch()
 
     print(stop_info)
 
-    image = Image.new("RGB", (64, 32))
-    draw = ImageDraw.Draw(image)
-    i = 0
-    for k, v in stop_info.items():
-        text = "%(line)s %(direction)s" % v
-        times_text = ", ".join([minutes_to_text(time) for time in v["next_train_times"]])
-        draw.text((0,i*8-1), text + ": " + times_text, (200, 200, 200), font = font)
-        i = i + 1
+    # image = Image.new("RGB", (64, 32))
+    # draw = ImageDraw.Draw(image)
+    # i = 0
+    # for k, v in stop_info.items():
+    #     text = "%(line)s %(direction)s" % v
+    #     times_text = ", ".join([minutes_to_text(time) for time in v["next_train_times"]])
+    #     draw.text((0,i*8-1), text + ": " + times_text, (200, 200, 200), font = font)
+    #     i = i + 1
 
-    image.save("out.ppm", "PPM")
+    # image.save("out.ppm", "PPM")
 
 if __name__ == "__main__":
     sys.exit(main())
--- subway_time/mta_subway_fetcher.py
+++ subway_time/mta_subway_fetcher.py
@@ -5,44 +5,57 @@ from google.transit import gtfs_realtime_pb2
 import requests
 from .nyct_subway_pb2 import *
 import math
+import copy
+import time
+import threading
 
 class MTASubwayFetcher:
     def __init__(self, api_key, feed_ids, stop_ids):
         self.api_key = api_key
         self.feed_ids = feed_ids
         self.stop_ids = stop_ids
-
-    def fetch(self):
-        stop_info = {}
+        self.init_data = {}
         with open(get_resource("stops.csv"), "r") as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
                 if row['stop_id'] in self.stop_ids:
-                    stop_info[row['stop_id']] = {"name": row['stop_name']}
+                    self.init_data[row['stop_id']] = {"name": row['stop_name']}
+        self.data = self.init_data
+        self.last_query_time = time.time()
+        t = threading.Thread(target = self.fetch, daemon = true)
+        t.start()
 
-        for feed_id in self.feed_ids:
-            current_time = datetime.datetime.now()
-            feed = gtfs_realtime_pb2.FeedMessage()
-            response = requests.get("http://datamine.mta.info/mta_esi.php?key=%s&feed_id=%d" % (self.api_key, feed_id))
-            feed.ParseFromString(response.content)
-            for entity in feed.entity:
-                if entity.trip_update:
-                    for update in entity.trip_update.stop_time_update:
-                        if update.stop_id in self.stop_ids:
-                            stop_info[update.stop_id]["line"] = entity.trip_update.trip.route_id
-                            nyct_extension = entity.trip_update.trip.Extensions[nyct_trip_descriptor]
-                            stop_info[update.stop_id]["direction"] = self.get_direction(nyct_extension)
-                            time = update.arrival.time
-                            if time <= 0:
-                                time = update.departure.time
-                            time = datetime.datetime.fromtimestamp(time)
-                            time = math.trunc(((time - current_time).total_seconds()) / 60)
-                            times = stop_info[update.stop_id].get("next_train_times", [])
-                            times.append(time)
-                            stop_info[update.stop_id]["next_train_times"] = times
-        return stop_info
 
-    def get_direction(self, ext):
+    def fetch(self):
+        while True:
+            stop_info = copy.deepcopy(self.init_data)
+            for feed_id in self.feed_ids:
+                current_time = datetime.datetime.now()
+                feed = gtfs_realtime_pb2.FeedMessage()
+                response = requests.get("http://datamine.mta.info/mta_esi.php?key=%s&feed_id=%d" % (self.api_key, feed_id))
+                feed.ParseFromString(response.content)
+                for entity in feed.entity:
+                    if entity.trip_update:
+                        for update in entity.trip_update.stop_time_update:
+                            if update.stop_id in self.stop_ids:
+                                stop_info[update.stop_id]["line"] = entity.trip_update.trip.route_id
+                                nyct_extension = entity.trip_update.trip.Extensions[nyct_trip_descriptor]
+                                stop_info[update.stop_id]["direction"] = MTASubwayFetcher.get_direction(nyct_extension)
+                                time = update.arrival.time
+                                if time <= 0:
+                                    time = update.departure.time
+                                time = datetime.datetime.fromtimestamp(time)
+                                time = math.trunc(((time - current_time).total_seconds()) / 60)
+                                times = stop_info[update.stop_id].get("next_train_times", [])
+                                times.append(time)
+                                stop_info[update.stop_id]["next_train_times"] = times
+
+            self.last_query_time = time.time()
+            self.data = stop_info
+            time.sleep(60)
+
+    @staticmethod
+    def get_direction(ext):
         return {ext.NORTH: "North",
                 ext.EAST: "East",
                 ext.SOUTH: "South",
